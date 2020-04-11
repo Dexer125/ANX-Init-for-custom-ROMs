12397a4e8fb781b19341a6c81a089cfc5db704f9
Timotej Kusy <timotejkusyofficial@gmail.com>
2020-04-11 10:13:39 +0200

    Removed rm -rf as not needed anymore

--- anx.sh
+++ anx.sh
@@ -3,20 +3,20 @@
 DIR="/sdcard/.ANXCamera/"
 
 if [ ! -d "$DIR" ]; then
-	rm -rf /sdcard/.ANXCamera/cheatcodes/
+
 	mkdir -pv /sdcard/.ANXCamera/cheatcodes
 	cp -Rv /system/etc/ANXCamera/cheatcodes/ /sdcard/.ANXCamera/cheatcodes
 
-	rm -rf /sdcard/.ANXCamera/cheatcodes_reference/
+	
 	mkdir -p /sdcard/.ANXCamera/cheatcodes_reference
 	cp -R /system/etc/ANXCamera/cheatcodes/ /sdcard/.ANXCamera/cheatcodes_reference
 
 
-	rm -rf /sdcard/.ANXCamera/features/
+	
 	mkdir -p /sdcard/.ANXCamera/features
 	cp -R /system/etc/device_features/ /sdcard/.ANXCamera/features
 
-	rm -rf /sdcard/.ANXCamera/features_reference/
+	
 	mkdir -p /sdcard/.ANXCamera/features_reference
 	cp -R /system/etc/device_features/ /sdcard/.ANXCamera/features_reference
 	
